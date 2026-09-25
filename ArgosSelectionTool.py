9570f238dfa18480048fb1b67f7eb45a011a6c2c
peshetler <peyton.shetler@duke.edu>
2026-09-25 19:40:26 -0400

    Task 7: Process lines with while loop

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -21,12 +21,14 @@ the_box = {
 file_name = 'data/raw/Satellite tracking of black-capped petrels 2019-argos.csv'
 
 #Read the contents of the file into a list of lines
-with open(file_name,'r') as f:
-    #Read contents of file into a list
-	line_list = f.readlines()
+f = open(file_name,'r')
+#Read the headerline
+headerline = f.readline()
+#Read contents of file into a list
+lineString = f.readline()
 
 #Pretend we read one line of data from the file
-for lineString in line_list[1:]:
+while lineString != "":
 
     # Use the split command to parse the items in lineString into a list object
     line_data = lineString.split(',')
@@ -35,9 +37,11 @@ for lineString in line_list[1:]:
     event_id = line_data[0]   # Argos tracking event ID ("event-id")
     timestamp = line_data[2]  # Observation date ("timestamp")
     lc  = line_data[14]        # Observation location class ("argos:lc")
-    if not lc in ('"1"','"2"','"3"'):  continue
-    lat = float(line_data[4])        # Observation latitude  ("location-lat")
-    lon = float(line_data[3])        # Observation longitude ("location-lon")
+    if lc not in ('"1"','"2"','"3"'):
+        lineString = f.readline()
+        continue
+    lat = float(line_data[3])        # Observation latitude  ("location-lat")
+    lon = float(line_data[4])        # Observation longitude ("location-lon")
     tag_id = line_data[-3]     # Tag identifier ("tag-local-identifier")
     
     #Evaluate latitude and longitude conditions
@@ -49,3 +53,9 @@ for lineString in line_list[1:]:
         print(f'Record {event_id}: {tag_id} was IN the box at {timestamp}')
     else:
         print(f'Record {event_id}: {tag_id} was NOT IN the box at {timestamp}')
+
+    #Update line string
+    lineString = f.readline()   
+
+#Close the file
+f.close()
