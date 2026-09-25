ee1ec496ed4fb7f07d1c6e988334400c47934d00
peshetler <peyton.shetler@duke.edu>
2026-09-25 17:58:58 -0400

    Add side quest 1

---
+++ sidequest1.py
@@ -0,0 +1,4 @@
+#Set a variable to the CSV filename
+the_filename = 'data/raw/Satellite tracking of black-capped petrels 2019-argos.csv'
+#Create a file object pointing to file name
+f = open(the_filename,'r')
